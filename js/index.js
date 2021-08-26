3c90c20e918a52a92f02a7154704ac777fc51250
Hapo Ducdv <duc.dv@haposoft.com>
2021-08-26 15:45:33 +0700

    Fix errors spaces and documment ready

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
-﻿$('.feedback-slide').slick({
+﻿$(document).ready(function () {
+  $('.feedback-slide').slick({
     dots: true,
     infinite: false,
     speed: 300,
@@ -7,26 +8,27 @@
     prevArrow: '<button class="slide-arrow prev-arrow"><i class="fas fa-angle-left"></i></button>',
     nextArrow: '<button class="slide-arrow next-arrow"><i class="fas fa-angle-right"></i></button>',
     responsive: [
-        {
-            breakpoint: 1024,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1,
-                infinite: true,
-                dots: true
-            }
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
         }
+      }
     ]
-});
+  });
 
-$('.btn-close-msg').click(function() {
+  $('.btn-close-msg').click(function() {
     $('#messenger-show', this).removeClass('chatbox');
-});
+  });
 
-$('.btn-close-msg').click(function() {
+  $('.btn-close-msg').click(function() {
     $('#messenger-show').slideUp();
-});
+  });
 
-$('.logo-messenger').click(function() {
+  $('.logo-messenger').click(function() {
     $('#messenger-show').slideDown('chatbox');
+  });
 });
